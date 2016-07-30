13181d37d4b5d9a769e0849526ba39ccad600e06
bedisdover <sym14@software.nju.edu.cn>
2016-07-30 17:19:13 +0800

    refactor

--- Code/Executor/src/main/webapp/js/check.js
+++ Code/Executor/src/main/webapp/js/check.js
@@ -6,17 +6,34 @@
 
 $(function () {
     checkEmpty();
+    checkPassword();
 });
 
 /**
  * 检查表单是否为空
  */
 function checkEmpty() {
-    $('#form').find('input').on('focusout', function () {
+    $(':input').on('focusout', function () {
         if ($(this).val().trim() == '') {
             $(this).addClass('error').attr({
                 'data-toggle': 'tooltip',
                 'title': $(this).attr('placeholder') + '不能为空'
+            }).tooltip();
+        } else {
+            $(this).removeClass('error').tooltip('destroy');
+        }
+    });
+}
+
+/**
+ * 检查密码是否一致
+ */
+function checkPassword() {
+    $('#confirmPassword').on('focusout', function () {
+        if ($(this).val().trim() != $('#password').val().trim()) {
+            $(this).addClass('error').attr({
+                'data-toggle': 'tooltip',
+                'title': '密码不一致'
             }).tooltip();
         } else {
             $(this).removeClass('error').tooltip('destroy');
