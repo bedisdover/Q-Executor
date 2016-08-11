42ef5581b5765388e54f4efeebd0f668a8001a3a
KingsleyAdamWang <547212796@qq.com>
2016-08-11 17:36:30 +0800

    Development:StockNowTimeDao虽然实现了，但是还没测试,我的测试跑不了……调调

--- Code/Executor/src/main/java/cn/edu/nju/software/dao/StockNowTimeDao.java
+++ Code/Executor/src/main/java/cn/edu/nju/software/dao/StockNowTimeDao.java
@@ -1,9 +1,13 @@
 package cn.edu.nju.software.dao;
 
+import cn.edu.nju.software.utils.StringUtil;
+import cn.edu.nju.software.utils.TimeUtil;
 import cn.edu.nju.software.vo.StockNowTimeVO;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -19,6 +23,59 @@ public class StockNowTimeDao {
     BaseDao baseDao;
 
     public List<StockNowTimeVO> getNowTime(String codeNum, Date date){
-        return null;
+        List<StockNowTimeVO> stockNowTimeVOs = new ArrayList<StockNowTimeVO>();
+        codeNum = getCode(codeNum);//怕只传进来一个六位数代码 当然8位数最好
+        int hashcode = getHashNum(codeNum.substring(2));
+        //
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.DATE,1);
+        //
+        List<Object[]> datas = baseDao.execSqlQuery("select "+ StringUtil.CURRENT+","+StringUtil.TIME+" from "+StringUtil.STOCKS_INSTANCE+hashcode+" where "+StringUtil.CODE+
+        "=\""+codeNum+"\" and "+StringUtil.TIME+">"+date.getTime()+" and "+StringUtil.TIME+"<"+calendar.getTime().getTime()+" order by "+StringUtil.TIME);
+
+        //如果没有数据 获取前一天的返回
+        if (datas.size()==0){
+            calendar.setTime(date);
+            calendar.add(Calendar.DATE,-1);
+            return getNowTime(codeNum,calendar.getTime());
+        }
+
+        float avg_temp = new Float(0.0);
+        for (Object[] object : datas){
+            float current = Float.parseFloat(object[0].toString());
+            Date time = new Date(Long.parseLong(object[1].toString()));
+//            if(stockNowTimeVOs.size()==0){
+//                avg_temp = current;
+//            }
+//
+            avg_temp = (avg_temp*stockNowTimeVOs.size()+current)/(stockNowTimeVOs.size()+1);
+            stockNowTimeVOs.add(new StockNowTimeVO(TimeUtil.getDetailTime(date),current,avg_temp));
+
+        }
+
+        return stockNowTimeVOs;
+
+    }
+
+
+    public String getCode(String code){
+        if(code.length()==8){
+            return code;
+        }
+
+        if (code.length()==6){
+            if (code.charAt(0)=='6'){
+                code="sh"+code;
+            }else {
+                code = "sz"+code;
+            }
+        }
+        return code;
+    }
+
+    public int getHashNum(String code){
+        int result = Integer.parseInt(code);
+        return  result%25;
     }
 }
--- Code/Executor/src/main/test/StockBasicInfoTest.java
+++ Code/Executor/src/main/test/StockBasicInfoTest.java
@@ -1,6 +1,9 @@
 import cn.edu.nju.software.dao.StockBasicInfoDao;
+import cn.edu.nju.software.dao.StockNowTimeDao;
 import cn.edu.nju.software.dao.UserDao;
 import cn.edu.nju.software.model.StockBasicInfo;
+import cn.edu.nju.software.utils.TimeUtil;
+import cn.edu.nju.software.vo.StockNowTimeVO;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -19,6 +22,8 @@ import java.util.Objects;
 public class StockBasicInfoTest {
     @Resource
     StockBasicInfoDao stockDao;
+    @Resource
+    StockNowTimeDao stockNowTimeDao;
     @Test
     public void test(){
         //test获取所有的股票的基本信息的对象
@@ -60,6 +65,15 @@ public class StockBasicInfoTest {
         List<StockBasicInfo> temp = stockDao.getStocksBasicInfoByArea("上海");
         for(StockBasicInfo info : temp){
             System.out.println(info);
+        }
+    }
+
+
+    @Test
+    public void test6(){
+        List<StockNowTimeVO> result = stockNowTimeDao.getNowTime("sh600000", TimeUtil.getDate("2016-08-10"));
+        for (StockNowTimeVO stockNowTimeVO : result){
+            System.out.println(stockNowTimeVO);
         }
     }
 }
