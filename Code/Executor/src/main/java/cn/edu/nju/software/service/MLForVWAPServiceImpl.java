b3a15566c3a9759a0fb4cf8057f2a5e853aec34a
Lemonstars <141250079@smail.nju.edu.cn>
2016-08-16 20:40:21 +0800

    ...

--- Code/Executor/src/main/java/cn/edu/nju/software/service/MLForVWAPServiceImpl.java
+++ Code/Executor/src/main/java/cn/edu/nju/software/service/MLForVWAPServiceImpl.java
@@ -1,6 +1,8 @@
 package cn.edu.nju.software.service;
 
 import cn.edu.nju.software.vo.MLForVWAPPriceVO;
+import libsvm.svm_model;
+import libsvm.svm_problem;
 
 import java.util.ArrayList;
 
@@ -8,6 +10,13 @@ import java.util.ArrayList;
  * Created by admin on 2016/8/16.
  */
 public class MLForVWAPServiceImpl implements MLForVWAPService {
+
+    private StockMLService stockService;
+    private svm_problem problem;     //定义svm_problem对象
+    private svm_problem param  ;    //svm参数
+    private svm_model model;       //训练的svm模型
+
+
 
     //返回最新数据下动态预测的均价
     public MLForVWAPPriceVO getDynamicPrice(String stockID){
