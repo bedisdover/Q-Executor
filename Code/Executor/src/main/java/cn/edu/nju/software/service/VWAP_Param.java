495833804959ad6aae90dcbc36d59fd3d2a32199
zyf14 <zyf14@software.nju.edu.cn>
2016-08-24 23:37:55 +0800

    VWAP算法实现上传

---
+++ Code/Executor/src/main/java/cn/edu/nju/software/service/VWAP_Param.java
@@ -0,0 +1,65 @@
+package cn.edu.nju.software.service;
+
+/**
+ * @author ZhangYF
+ *
+ */
+public class VWAP_Param {
+
+	//用户交易量
+	private long userVol;
+	
+	//股票代码
+	private String stockid;
+	
+	//
+	private double delta;
+	
+	//当前时间段
+	private int timeNode;
+	
+	//总时间段数量
+	private int timeSliceNum;
+
+	public long getUserVol() {
+		return userVol;
+	}
+
+	public void setUserVol(long userVol) {
+		this.userVol = userVol;
+	}
+
+	public String getStockid() {
+		return stockid;
+	}
+
+	public void setStockid(String stockid) {
+		this.stockid = stockid;
+	}
+
+	public double getDelta() {
+		return delta;
+	}
+
+	public void setDelta(double delta) {
+		this.delta = delta;
+	}
+
+	public int getTimeNode() {
+		return timeNode;
+	}
+
+	public void setTimeNode(int timeNode) {
+		this.timeNode = timeNode;
+	}
+
+	public int getTimeSliceNum() {
+		return timeSliceNum;
+	}
+
+	public void setTimeSliceNum(int timeSliceNum) {
+		this.timeSliceNum = timeSliceNum;
+	}
+	
+	
+}
