6e07baa78b43f79f3b0ebc262f7c04bffb811e85
Lemonstars <141250079@smail.nju.edu.cn>
2016-08-27 16:32:13 +0800

    Clinet -svm

---
+++ Code/Client/src/main/java/service/MLForVWAPService.java
@@ -0,0 +1,19 @@
+package  service;
+
+import  vo.MLForVWAPPriceVO;
+
+import java.util.ArrayList;
+
+/**
+ * Created by LiuXing on 2016/8/8.
+ * @goal:VWAP算法获取机器学习部分预测的结果
+ */
+public interface MLForVWAPService {
+
+    //返回最新数据下动态预测的均价
+    public MLForVWAPPriceVO getDynamicPrice(String stockID);
+
+    //返回最新数据下静态预测的48个成交量
+    public ArrayList<Integer>   getStaticVol(String stockID);
+
+}
