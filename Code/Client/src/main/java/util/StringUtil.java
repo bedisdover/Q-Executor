563527600b9ddeb8d9a44d13e7c6462a69901f80
KingsleyAdamWang <547212796@qq.com>
2016-08-27 23:07:12 +0800

    添加StcokMLServiceImpl以及其相关文件

---
+++ Code/Client/src/main/java/util/StringUtil.java
@@ -0,0 +1,43 @@
+package util;
+
+/**
+ * Created by 王栋 on 2016/8/10 0010.
+ */
+public class StringUtil {
+    public static final String CURRENT = "current";
+    public static final String TIME = "time";
+    public static final String STOCKS_INFO = "StocksInfo";
+    public static final String STOCKS_INSTANCE = "StocksInstance";
+    public static final String CODE = "code";
+    public static final String NAME = "name";
+    public static final String INDUSTRY = "industry";
+    public static final String AREA = "area";
+    public static final String PE = "pe";
+    public static final String OUTSTANDING = "outstanding";
+    public static final String TOTALS = "totals";
+    public static final String TOTAL_ASSETS = "totalAssets";
+    public static final String LIQUID_ASSETS = "liquidAssets";
+    public static final String FIXED_ASSETS = "fixedAssets";
+    public static final String RESERVED = "reserved";
+    public static final String RESERVED_PER_SHARE = "reservedPerShare";
+    public static final String EPS ="eps";
+    public static final String BVPS ="bvps";
+    public static final String PB = "pb";
+    public static final String TIME_TO_MARKET="timeToMarket";
+    public static final String OPEN = "open";
+    public static final String CLOSE = "close";
+    public static final String HIGH = "high";
+    public static final String LOW = "low";
+    public static final String VOLUME = "volume";
+    public static final String DATE = "date";
+    public static final String PRICE_CHANGE="priceChange";
+    public static final String P_CHANGE = "pChange";
+    public static final String MA5 = "ma5";
+    public static final String MA10 = "ma10";
+    public static final String MA20 = "ma20";
+    public static final String V_MA5 = "v_ma5";
+    public static final String V_MA10 = "v_ma10";
+    public static final String V_MA20 = "v_ma20";
+    public static final String TURNOVER = "turnover";
+    public static final String HISTORY_5MIN_DATA = "History5minData";
+}
