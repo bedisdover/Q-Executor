9fc4d1eb0f83ed2abfcb4fe13103207569b3a6ea
bedisdover <sym14@software.nju.edu.cn>
2016-08-28 16:21:11 +0800

    改变交互方式

--- Code/Client/src/main/java/present/panel/stock/GeneralPanel.java
+++ Code/Client/src/main/java/present/panel/stock/GeneralPanel.java
@@ -10,121 +10,106 @@ import java.awt.*;
  * <p>
  * 大单面板
  */
-public class GeneralPanel extends StockPanel {
+public class GeneralPanel extends JPanel {
+
+    private JPanel panel;
+
+    private JLabel label;
+
+    private JRadioButton radio400, radio600, radio800, radio1000;
 
     public GeneralPanel(String stockCode) {
-        super(stockCode);
+
+
+        panel = this;
+
+        init();
+        createUIComponents();
     }
 
-    @Override
-    void createCenterPanel() {
+    private void init() {
         SwingUtilities.invokeLater(() -> {
-            centerPanel = new CenterPanel();
-            panel.add(centerPanel, BorderLayout.CENTER);
+            panel.setBackground(new Color(0xeeeeee));
+            panel.setLayout(new BorderLayout(0, 20));
+
+            panel.revalidate();
         });
     }
 
-    /**
-     * 中心面板
-     */
-    private static class CenterPanel extends JPanel {
-        private JPanel panel;
+    private void createUIComponents() {
+        SwingUtilities.invokeLater(() -> {
+            {
+                JPanel northPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 0));
 
-        private JLabel label;
+                label = new MyLabel("成交量大于等于(≥):");
 
-        private JRadioButton radio400, radio600, radio800, radio1000;
+                radio400 = new JRadioButton("400手");
+                radio600 = new JRadioButton("600手");
+                radio800 = new JRadioButton("800手");
+                radio1000 = new JRadioButton("1000手");
 
-        CenterPanel() {
-            panel = this;
+                ButtonGroup buttonGroup = new ButtonGroup();
 
-            init();
-            createUIComponents();
-        }
+                northPanel.add(label);
+                northPanel.add(radio400);
+                northPanel.add(radio600);
+                northPanel.add(radio800);
+                northPanel.add(radio1000);
+                buttonGroup.add(radio400);
+                buttonGroup.add(radio600);
+                buttonGroup.add(radio800);
+                buttonGroup.add(radio1000);
 
-        private void init() {
-            SwingUtilities.invokeLater(() -> {
-                panel.setBackground(new Color(0xeeeeee));
-                panel.setLayout(new BorderLayout(0, 20));
+                panel.add(northPanel, BorderLayout.NORTH);
+            }
 
-                panel.revalidate();
-            });
-        }
+            {
+                JPanel centerPanel = new JPanel(new BorderLayout());
 
-        private void createUIComponents() {
-            SwingUtilities.invokeLater(() -> {
-                {
-                    JPanel northPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 0));
+                centerPanel.setPreferredSize(new Dimension(1, 200));
 
-                    label = new MyLabel("成交量大于等于(≥):");
+                centerPanel.add(new GeneralPie().getPieChart(""), BorderLayout.WEST);
 
-                    radio400 = new JRadioButton("400手");
-                    radio600 = new JRadioButton("600手");
-                    radio800 = new JRadioButton("800手");
-                    radio1000 = new JRadioButton("1000手");
+                Box labelBox = Box.createVerticalBox();
 
-                    ButtonGroup buttonGroup = new ButtonGroup();
+                labelBox.add(new MyLabel("大单成交量:18.56万股"));
+                labelBox.add(new MyLabel("总成交量:441.34万股"));
+                labelBox.add(new MyLabel("总成交量:441.34万股"));
+                labelBox.add(new MyLabel("总成交量:441.34万股"));
+                labelBox.add(new MyLabel("总成交量:441.34万股"));
+                labelBox.add(new MyLabel("总成交量:441.34万股"));
 
-                    northPanel.add(label);
-                    northPanel.add(radio400);
-                    northPanel.add(radio600);
-                    northPanel.add(radio800);
-                    northPanel.add(radio1000);
-                    buttonGroup.add(radio400);
-                    buttonGroup.add(radio600);
-                    buttonGroup.add(radio800);
-                    buttonGroup.add(radio1000);
+                centerPanel.add(labelBox, BorderLayout.CENTER);
 
-                    panel.add(northPanel, BorderLayout.NORTH);
-                }
+                panel.add(centerPanel, BorderLayout.CENTER);
+            }
 
-                {
-                    JPanel centerPanel = new JPanel(new BorderLayout());
+            {
+                JPanel southPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 
-                    centerPanel.setPreferredSize(new Dimension(1, 200));
+                southPanel.setPreferredSize(new Dimension(1, 300));
 
-                    centerPanel.add(new GeneralPie().getPieChart(""), BorderLayout.WEST);
+                southPanel.add(createTable());
 
-                    Box labelBox = Box.createVerticalBox();
+                panel.add(southPanel, BorderLayout.SOUTH);
+            }
+        });
+    }
 
-                    labelBox.add(new MyLabel("大单成交量:18.56万股"));
-                    labelBox.add(new MyLabel("总成交量:441.34万股"));
-                    labelBox.add(new MyLabel("总成交量:441.34万股"));
-                    labelBox.add(new MyLabel("总成交量:441.34万股"));
-                    labelBox.add(new MyLabel("总成交量:441.34万股"));
-                    labelBox.add(new MyLabel("总成交量:441.34万股"));
+    private JScrollPane createTable() {
+        Object[][] playerInfo = {
+                {"阿呆", 66, 32, 98, false},
+                {"阿呆", 82, 69, 128, true},
+                {"阿呆", 82, 69, 128, true},
+                {"阿呆", 82, 69, 128, true},
+                {"阿呆", 82, 69, 128, true},
+                {"阿呆", 82, 69, 128, true},
+                {"阿呆", 82, 69, 128, true},
+        };
 
-                    centerPanel.add(labelBox, BorderLayout.CENTER);
+        //字段名称
+        String[] Names = {"交易时间", "成交价", "成交量(手)", "成交额(万元)", "买卖盘性质"};
 
-                    panel.add(centerPanel, BorderLayout.CENTER);
-                }
-
-                {
-                    JPanel southPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-
-                    southPanel.setPreferredSize(new Dimension(1, 300));
-
-                    southPanel.add(createTable());
-
-                    panel.add(southPanel, BorderLayout.SOUTH);
-                }
-            });
-        }
-
-        private JScrollPane createTable() {
-            Object[][] playerInfo = {
-                    {"阿呆", 66, 32, 98, false},
-                    {"阿呆", 82, 69, 128, true},
-                    {"阿呆", 82, 69, 128, true},
-                    {"阿呆", 82, 69, 128, true},
-                    {"阿呆", 82, 69, 128, true},
-                    {"阿呆", 82, 69, 128, true},
-                    {"阿呆", 82, 69, 128, true},
-            };
-
-            //字段名称
-            String[] Names = {"交易时间", "成交价", "成交量(手)", "成交额(万元)", "买卖盘性质"};
-
-            return new MyTable(playerInfo, Names).createTable();
-        }
+        return new MyTable(playerInfo, Names).createTable();
     }
 }
--- Code/Client/src/main/java/present/panel/stock/SinglePanel.java
+++ Code/Client/src/main/java/present/panel/stock/SinglePanel.java
@@ -1,13 +1,15 @@
 package present.panel.stock;
 
+import javax.swing.*;
+
 /**
  * Created by Y481L on 2016/8/27.
  * <p>
  * 逐笔面板
  */
-public class SinglePanel extends StockPanel {
+public class SinglePanel extends JPanel {
 
     public SinglePanel(String stockCode) {
-        super(stockCode);
+
     }
 }
