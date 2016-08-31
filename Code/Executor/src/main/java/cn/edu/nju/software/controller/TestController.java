f179677da0fb1427b11928448e16f4c3f2e75a53
bedisdover <sym14@software.nju.edu.cn>
2016-08-31 13:08:48 +0800

    test price and volume

--- Code/Executor/src/main/java/cn/edu/nju/software/controller/TestController.java
+++ Code/Executor/src/main/java/cn/edu/nju/software/controller/TestController.java
@@ -1,8 +1,58 @@
 package cn.edu.nju.software.controller;
 
+import cn.edu.nju.software.service.MLForVWAPService;
+import cn.edu.nju.software.service.StockMLService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+
 /**
  * Created by song on 16-8-3.
+ * <p>
+ * 测试VWAP
  */
 public class TestController {
+    @Resource
+    MLForVWAPService mlForVWAPService;
 
+    @Resource
+    StockMLService stockMLService;
+
+    @RequestMapping("/testVolume")
+    @ResponseBody
+    public String testVolume() {
+        String all_stock[] = stockMLService.getStocksNeedCal();
+
+        String result = "";
+
+        for (String anAll_stock : all_stock) {
+            ArrayList<Integer> list = mlForVWAPService.getStaticVol(anAll_stock);
+
+            for (int i = 0; i < list.size(); i++) {
+                result += "index: " + (i + 1) + " " + list.get(i) + "\n";
+            }
+        }
+
+        return result;
+    }
+
+    @RequestMapping("/testPrice")
+    @ResponseBody
+    public String testPrice() {
+        String all_stock[] = stockMLService.getStocksNeedCal();
+
+        String result = "";
+
+        for (String stock: all_stock) {
+            ArrayList<Double> list = mlForVWAPService.getDynamicPrice(stock).getPriceList();
+
+            for (int i = 0; i < list.size(); i++) {
+                result += "index: " + (i + 1) + " " + list.get(i) + "\n";
+            }
+        }
+
+        return result;
+    }
 }
