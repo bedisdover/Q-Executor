bd2b5bb0e03e5bb10aa44131dc31c48f4178dc81
bedisdover <sym14@software.nju.edu.cn>
2016-09-09 21:51:05 +0800

    txt

---
+++ Code/Client/src/main/java/present/charts/listener/MyChartMouseAdapter.java
@@ -0,0 +1,35 @@
+package present.charts.listener;
+
+import org.jfree.chart.ChartMouseEvent;
+
+/**
+ * Created by song on 16-9-9.
+ *
+ * 自定义鼠标监听适配器
+ */
+public class MyChartMouseAdapter implements MyChartMouseListener {
+    @Override
+    public void chartMousePressed(ChartMouseEvent event) {
+
+    }
+
+    @Override
+    public void chartMouseReleased(ChartMouseEvent event) {
+
+    }
+
+    @Override
+    public void chartMouseExited(ChartMouseEvent event) {
+
+    }
+
+    @Override
+    public void chartMouseClicked(ChartMouseEvent event) {
+
+    }
+
+    @Override
+    public void chartMouseMoved(ChartMouseEvent event) {
+
+    }
+}
---
+++ Code/Client/src/test/java/SliderTest.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+/**
+ * Created by song on 16-9-9.
+ * 测试滑块
+ */
+public class SliderTest extends JFrame {
+    public static void main(String[] args) {
+        new SliderTest();
+    }
+
+    private SliderTest() {
+        try {
+            org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper.launchBeautyEyeLNF();
+        }
+        catch (Exception e) {
+            System.out.println("Look and feel Exception!");
+        }
+
+        setLayout(new FlowLayout());
+
+        JSlider slider = new JSlider(0, 100);
+//        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+//        slider.setPaintTrack(true);
+        slider.setMajorTickSpacing(10);
+        slider.setSnapToTicks(true);
+        add(slider);
+
+        slider.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                System.out.println("SliderTest.mouseEntered");
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println("SliderTest.mouseClicked");
+            }
+        });
+
+        setSize(400, 400);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
